e84b373a0f7026dd240ca863729a3e11ab8fa439
daxter-army <mehulsaini456@gmail.com>
2022-12-28 22:23:26 +0530

    added dashboard app

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react"
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 import { mount } from "auth/AuthApp"
 
 export default ({ onSignIn }) => {
